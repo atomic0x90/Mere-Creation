04d1ebb7bf382eaf72fdf2ab2c97b6a416c45bce
atomic0x90 <yoojun20@gmail.com>
2019-08-02 22:33:44 +0900

    Test access function

--- Find_a_file/Find_a_file.cpp
+++ Find_a_file/Find_a_file.cpp
@@ -1,4 +1,31 @@
 #include <iostream>
-
+#include <unistd.h>
+#include <string>
+#include <cstdlib>
+/*
+ * int access(const char *path, int amode);
+ *  - Header file(unistd.h)
+ *
+ *
+*/
 
 using namespace std;
+
+
+string a;
+int main()
+{
+	getline(cin,a);
+	const char *path = a.c_str();
+
+	int num = access(path,0);
+
+	cout<<num<<endl;
+	
+	getline(cin,a);
+	path = a.c_str();
+	num = access(path,0);
+
+	cout<<num<<endl;
+	return 0;
+}
