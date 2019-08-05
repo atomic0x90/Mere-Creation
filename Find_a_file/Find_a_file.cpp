4a7b4385ece0a780b1b74af92d99ab7ecbc7b20c
atomic0x90 <yoojun20@gmail.com>
2019-08-05 23:33:57 +0900

    Add comments access function

--- Find_a_file/Find_a_file.cpp
+++ Find_a_file/Find_a_file.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include <unistd.h>
+#include <unistd.h>	//Using 'access(const char*, int)'
 #include <string>
-#include <cstdlib>
+#include <cstdlib>	//Using 'c_str()'
 /*
  * int access(const char *path, int amode);
  *  - Header file(unistd.h)
+ *  - Ability to check if a file exists
+ *  - path : Path to file
+ *  - amode : Type of inspection
+ *    - 0 : Check the existence of a file
+ *    - 1 : Running a file
+ *    - 2 : Check if file can be written
+ *    - 4 : Check if file can be read
+ *    - 6 : Check if file can be written and read
+ *  - Return value
+ *    - true : 0
+ *    - false(error) : -1
  *
  *
 */
