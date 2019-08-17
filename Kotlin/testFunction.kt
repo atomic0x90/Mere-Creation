f1dac278416abf5b751b3ce47bdda92c59e67095
atomic0x90 <yoojun20@gmail.com>
2019-08-17 22:14:26 +0900

    Test function type of notation

--- Kotlin/testFunction.kt
+++ Kotlin/testFunction.kt
@@ -2,14 +2,18 @@ fun main()
 {
 	println("TEST function")
 	
-	var a:Int = 1
-	var b:Int = 2
+	var a:Int = 2
+	var b:Int = 3
 
 	println(sum(a,b))
 	
 }
-
+/*
 fun sum(a:Int,b:Int):Int
 {
 	return a+b;
-}
+}*/
+
+//fun sum(a:Int,b:Int):Int = a + b
+
+fun sum(a:Int,b:Int) = a + b
