f47157305b81b9d8857fff1b54c83f6ac281fe50
atomic0x90 <yoojun20@gmail.com>
2019-09-12 17:58:18 +0900

    Fix function

--- algorithmProblem/bowlingScore/bowling.cpp
+++ algorithmProblem/bowlingScore/bowling.cpp
@@ -7,14 +7,13 @@ ifstream finput("bowling.inp");
 ofstream foutput("bowling.out");
 
 int score[10][3];
-int testCase,tmp1,tmp2,tmp3,result,strikeCheck[10];
+int testCase,tmp1,tmp2,tmp3,result,strikeCheck[10],spareCheck[10];
 
 int main()
 {
 
 	finput >> testCase;
 
-	cout<<testCase<<endl;
 
 	while(testCase > 0)
 	{
@@ -36,7 +35,7 @@ int main()
 				if(score[i][0] == 10 || score[i][0] + score[i][1] == 10)
 				{
 					strikeCheck[i]++;
-
+					
 					for(int j = i-1; j >= 0; j--)
 					{
 						if(strikeCheck[j] == 0)
@@ -45,7 +44,6 @@ int main()
 							strikeCheck[j]++;
 					}
 				}
-
 			}
 			else
 			{
@@ -53,7 +51,7 @@ int main()
 				score[i][0] = tmp1;
 				score[i][1] = tmp2;
 
-				if(( (tmp1+tmp2) % 10 == 0) && (tmp1+tmp2) != 0)
+				if((tmp1 == 10 || (tmp1 + tmp2) % 10 == 0) && (tmp1+tmp2) != 0)
 				{
 					finput >> tmp3;
 					score[i][2] = tmp3;
@@ -69,18 +67,33 @@ int main()
 				{
 					result += score[i][0] + score[i][1];
 				}
-				else if(score[i][0] == 10)	//strike
+				else if(strikeCheck[i] != 0)
 				{
-					if(strikeCheck[i] == 1)
-						result += score[i][0] + score[i+1][1] + score[i+1][0];
-					else if(strikeCheck[i] == 2)
-						result += score[i][0] + score[i+1][0] + score[i+1][1];
-					else if(strikeCheck[i] >= 3)
-						result += 30;
-				}
-				else if(score[i][0] != 10 && score[i][0] + score[i][1] == 10)	//spare
-				{
-					result += score[i][0] + score[i][1] + score[i+1][0];
+					if(score[i][0] == 10)	//strike
+					{
+						if(strikeCheck[i] >= 3)
+						{
+							if(score[i+1][1] == 0 && score[i+2][1] == 0)
+								result += 30;
+							else if(score[i+1][1] == 0 && score[i+2][1] != 0)
+								result += 20 + score[i+2][0];
+							else if(score[i+1][1] != 0)
+								result += 20;
+						}
+						else if(strikeCheck[i] == 2)
+						{
+							if(score[i+1][1] == 0)
+								result += 20 + score[i+2][0];
+							else if(score[i+1][1] != 0)
+								result += 20;
+						}
+						else
+							result += 10 + score[i+1][0] + score[i+1][1];
+					}
+					else	//spare
+					{
+						result += 10 + score[i+1][0];
+					}
 				}
 			}
 			else
@@ -88,21 +101,12 @@ int main()
 				result += score[i][0] + score[i][1] + score[i][2];
 			}
 
-			cout<<"result: "<<result<<endl;
 		}
-/*
-		for(int i = 0; i < 10; i++)
-		{
-			cout<<score[i][0]<<" "<<score[i][1]<<" "<<score[i][2]<<endl;
-		}*/
 
-		for(int i = 0; i < 10; i++)
-			cout<<strikeCheck[i]<<endl;
-
-		cout<<endl<<result<<endl;
+		foutput<<result<<endl;
 
 		for(int i = 0; i < 10; i++)	// array initial
-			score[i][0] = score[i][1] = score[i][2] = strikeCheck[i] = 0;
+			score[i][0] = score[i][1] = score[i][2] = strikeCheck[i] = spareCheck[i] = 0;
 		
 		testCase--;
 	}
