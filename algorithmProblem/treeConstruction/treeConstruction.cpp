dde89ab9b66158eb8f2bb517e3af9a47acc1f37f
atomic0x90 <yoojun20@gmail.com>
2019-09-16 20:12:35 +0900

    Initial state

---
+++ algorithmProblem/treeConstruction/treeConstruction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream fin("tree.inp");
+ofstream fout("tree.out");
+
+int main()
+{
+
+	fin.close();
+	fout.close();
+
+	return 0;
+}
