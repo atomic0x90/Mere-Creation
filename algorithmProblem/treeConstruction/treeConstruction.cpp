b21d802e97f1858d75e86799e063daf705acdbb8
atomic0x90 <yoojun20@gmail.com>
2019-09-17 20:26:32 +0900

    Add function type, finFunction

--- algorithmProblem/treeConstruction/treeConstruction.cpp
+++ algorithmProblem/treeConstruction/treeConstruction.cpp
@@ -1,13 +1,59 @@
 #include <iostream>
 #include <fstream>
 
+#include <cstring>
+
 using namespace std;
 
 ifstream fin("tree.inp");
 ofstream fout("tree.out");
 
+int testCase, checkLength;
+
+string finString;
+
+char *splitString;
+
+void finFunction();
+void foutFunction();
+void treeConstruction();
+
+void finFunction()
+{
+	getline(fin,finString);
+
+	splitString = (char *)finString.c_str();
+	
+	checkLength = strlen(splitString);
+
+	cout<<checkLength<<endl;
+
+	return;
+}
+
+void treeConstruction()
+{
+
+	return;
+}
+
+void foutFunction()
+{
+
+	return;
+}
+
 int main()
 {
+	fin >> testCase;
+
+
+	while(testCase >= 0)
+	{
+		finFunction();
+
+		testCase--;
+	}
 
 	fin.close();
 	fout.close();
