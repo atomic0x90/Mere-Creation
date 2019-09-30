f6bbec669e28a3a3282f42c6e9bb964fb8f11eb8
atomic0x90 <yoojun20@gmail.com>
2019-09-30 18:48:49 +0900

    Initial state

---
+++ algorithmProblem/colorPaper/paper.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream fin("paper.inp");
+ofstream fout("paper.out");
+
+int main()
+{
+
+	fin.close();
+	fout.close();
+
+	return 0;
+}
