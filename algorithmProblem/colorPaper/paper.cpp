27fef70020f8d7d0dc55829dc8b47d2cb496b31b
atomic0x90 <yoojun20@gmail.com>
2019-10-01 20:35:34 +0900

    Add init function, vectorSet function

--- algorithmProblem/colorPaper/paper.cpp
+++ algorithmProblem/colorPaper/paper.cpp
@@ -1,13 +1,51 @@
 #include <iostream>
 #include <fstream>
 
+#include <vector>
+#include <utility>
+
 using namespace std;
 
 ifstream fin("paper.inp");
 ofstream fout("paper.out");
 
+vector < pair <int,int> > xAxisCoordinates;
+vector < pair <int,int> > yAxisCoordinates;
+
+vector <int> invisibleDegree;
+
+int testCase;
+
+
+void vectorSet();
+void finFunction();
+void verifyOverlapFunction();
+void foutFunction();
+
+void vectorSet()
+{
+	xAxisCoordinates.reserve(testCase);
+	yAxisCoordinates.reserve(testCase);
+	invisibleDegree.reserve(testCase);
+
+	return ;
+}
+
 int main()
 {
+	fin >> testCase;
+
+	cout<<testCase<<endl;
+
+	vectorSet();	
+
+	cout<<xAxisCoordinates.size()<<" "<<yAxisCoordinates.size()<<" "<<invisibleDegree.size()<<endl;
+
+	while(testCase)
+	{
+		
+		testCase--;
+	}
 
 	fin.close();
 	fout.close();
