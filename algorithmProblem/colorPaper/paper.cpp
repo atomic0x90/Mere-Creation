850c44cc2b33d90c2f3b7ab327fc21509cb772f2
atomic0x90 <yoojun20@gmail.com>
2019-10-02 21:55:26 +0900

    Add bool (xF,xFC,etc..)

--- algorithmProblem/colorPaper/paper.cpp
+++ algorithmProblem/colorPaper/paper.cpp
@@ -24,9 +24,9 @@ void foutFunction();
 
 void vectorSet()
 {
-	xCoordinates.reserve(testCase);
-	yCoordinates.reserve(testCase);
-	invisibleDegree.reserve(testCase);
+	xCoordinates.reserve(testCase * 2);
+	yCoordinates.reserve(testCase * 2);
+	invisibleDegree.reserve(testCase * 2);
 
 	return ;
 }
@@ -54,6 +54,9 @@ void finFunction()
 		i++;
 		testCase--;
 	}
+
+	for(int j = 0 ; j < i ; j++)
+		cout<< invisibleDegree[j]<<endl;
 	
 	return ;
 }
@@ -64,12 +67,25 @@ void verifyOverlapFunction(int value,int x,int w,int y,int h)
 
 	for(; i < j ; i++)
 	{
+		bool xF = xCoordinates[i].first < x;	//입력된 x 좌표가 이미 있는 좌표보다 큰 경우 1
+		bool xFC = xCoordinates[i].first == x;	//x 좌표가 같은 경우 1
+		bool xS = xCoordinates[i].second < w;	//입력된 x+w 좌표가 이미 있는 좌표보다 큰 경우 1
+		bool xSC = xCoordinates[i].second == w;	//x+w 좌표가 같은 경우 1
+		bool yF = yCoordinates[i].first < y;
+		bool yFC = yCoordinates[i].first == y;
+		bool yS = yCoordinates[i].second < h;
+		bool ySC = yCoordinates[i].second == h;
+		cout<<x<<" "<<w<<" "<<y<<" "<<h<<" "<<xF<<endl;
 		if(invisibleDegree[i] != 2)
 		{
-			if( (xCoordinates[i].first > x && xCoordinates[i].second < w))
+			if((!xF || xFC) && (!yF || yFC) && (xS || XSC) && (yS || ySC))
+			{	//아예 덮을때
+				invisibleDegree[i] = 2;
+			}
+			else if( (xCoordinates[i].first >= x && xCoordinates[i].second >= w) )
 			{}
 		}
-	}
+	}cout<<endl;
 
 	return ;
 }
