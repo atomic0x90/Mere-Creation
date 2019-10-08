5279aca3cdaa3da04eb20f3a57c85f6c1903bb17
atomic0x90 <yoojun20@gmail.com>
2019-10-08 20:50:03 +0900

    Add checkPainting condition

--- algorithmProblem/cubePainting/cube.cpp
+++ algorithmProblem/cubePainting/cube.cpp
@@ -76,7 +76,7 @@ int checkEOFFunction()
 
 int checkPainting()
 {
-	int check = 0,tmp,tmp2;
+	int check = 0;
 
 	firstCube[0] = secondCube[0] = ' ';
 
@@ -86,20 +86,60 @@ int checkPainting()
 		secondCube[i+1] = splitString[6+i];
 	}
 
-	for(int i = 1; i < 7;i++)
+	while(check < 6)
 	{
-		if(firstCube[1] == secondCube[i] && firstCube[6] == secondCube[7-i])
+		for(int j = 1;j < 4;j++)
 		{
-			check = i;
-			break;
+			int tmp = 0;
+			for(int i = check + 1; i < 7;i++)
+			{
+				if(firstCube[j] == secondCube[i] && firstCube[7-j] == secondCube[7-i])
+				{
+					tmp  = 1;
+					check = i;
+					break;
+				}
+			}
+			if(tmp == 1)
+				break;
 		}
+	
+		if(check == 0)
+			return 0;
+		else
+			cubeSpin(check);
+	
+		if(firstCube[2] == secondCube[2] && firstCube[5] == secondCube[5])
+		{
+			if(firstCube[3] == secondCube[3] && firstCube[4] == secondCube[4])
+				return 1;
+		}
+		else if(firstCube[2] == secondCube[5] && firstCube[5] == secondCube[2])
+		{
+			if(firstCube[3] == secondCube[4] && firstCube[4] == secondCube[3])
+				return 1;
+		}
+		else if(firstCube[2] == secondCube[4] && firstCube[5] == secondCube[3])
+		{
+			if(firstCube[3] == secondCube[5] && firstCube[4] == secondCube[5])
+				return 1;
+		}
+		else if(firstCube[2] == secondCube[3] && firstCube[5] == secondCube[4])
+		{
+			if(firstCube[3] == secondCube[5] && firstCube[4] == secondCube[5])
+				return 1;
+		}
+
+		for(int i = 1;i<7;i++)
+			cout<<firstCube[i];
+		cout<<"\t"<<check<<"\t";
+
+		for(int i = 1;i<7;i++)
+			cout<<secondCube[i];
+		cout<<endl;
+	
+		check++;
 	}
-
-	if(check == 0)
-		return 0;
-	else
-		cubeSpin(check);
-
 	return 0;
 }
 
@@ -109,47 +149,6 @@ void cubeSpin(int check)
 
 	if(check == 1)
 		return;
-/*	else if(check == 2)
-	{
-		tmp1 = secondCube[2];
-		tmp2 = secondCube[5];
-
-		secondCube[2] = secondCube[6];
-		secondCube[5] = secondCube[1];
-
-		secondCube[6] = tmp2;
-		secondCube[1] = tmp1;
-	}
-	else if(check == 3)
-	{
-		tmp1 = secondCube[3];
-		tmp2 = secondCube[4];
-
-		secondCube[3] = secondCube[6];
-		secondCube[4] = secondCube[1];
-
-		secondCube[6] = tmp2;
-		secondCube[1] = tmp1;
-	}
-	else if(check == 4)
-	{
-		tmp1 = secondCube[4];
-		tmp2 = secondCube[3];
-
-		secondCube[4] = secondCube[6];
-		secondCube[3] = secondCube[1];
-
-		secondCube[6] = tmp2;
-		secondCube[1] = tmp1;
-	}
-	else if(check == 5)
-	{
-		tmp1 = secondCube[5];
-		tmp2 = secondCube[2];
-
-		5 = 6;
-		2 = 1;
-	}*/
 	else if(check == 6)
 	{
 		tmp1 = secondCube[6];
