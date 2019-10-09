4671eae1102d46d1bd4048ede9299765285587a2
atomic0x90 <yoojun20@gmail.com>
2019-10-09 23:07:55 +0900

    Add test finFunction

--- algorithmProblem/treeConstructionByTraversals/tree2.cpp
+++ algorithmProblem/treeConstructionByTraversals/tree2.cpp
@@ -8,14 +8,66 @@ ofstream fout("tree.out");
 
 int nodeNum;
 int orderType1, orderType2;
-int *finPre, *finIn, *finPost;
+int finPre[10000] = {0,};
+int finIn[10000] = {0,};
+int finPost[10000] = {0,};
 
 void finFunction();
 void foutFunction();
 void constructionTree();
 
+void preorderPrint();
+void inorderPrint();
+void postorderPrint();
+
+typedef struct NodeStruct
+{
+	int value;
+	struct NodeStruct* left;
+	struct NodeStruct* right;
+}Node;
+
+Node *root = NULL;
+
+Node* insertNumber(Node* root,int data)
+{
+	root = (Node*)malloc(sizeof(Node));
+	root->left = NULL;
+	root->right = NULL;
+	root->value = data;
+
+	return root;
+}
+
+void finFunction()
+{
+	fin>>nodeNum;
+
+	fin>>orderType1;
+
+	cout<<orderType1<<endl;
+	if(orderType1 == -1)
+	{
+		for(int i = 0;i < nodeNum;i++)
+			fin>>finPre[i];
+	}
+	else if(orderType1 == 0)
+	{
+		for(int i = 0;i < nodeNum;i++)
+			fin>>finIn[i];
+	}
+	else if(orderType1 == 1)
+	{
+		for(int i = 0;i < nodeNum;i++)
+			fin>>finPost[i];
+	}
+
+	return;
+}
+
 int main()
 {
+	finFunction();
 
 	fin.close();
 	fout.close();
