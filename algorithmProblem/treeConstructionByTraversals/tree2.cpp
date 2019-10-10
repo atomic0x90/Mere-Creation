e7208761b6784a475a0d06d1da6bcceb23afe609
atomic0x90 <yoojun20@gmail.com>
2019-10-10 19:03:09 +0900

    Test insertNum

--- algorithmProblem/treeConstructionByTraversals/tree2.cpp
+++ algorithmProblem/treeConstructionByTraversals/tree2.cpp
@@ -14,6 +14,16 @@ Node* root = NULL;
 
 Node* insertNumber(Node* root,int data)
 {
+	if(root == NULL)
+	{
+		root = (Node*)malloc(sizeof(Node));
+		root->left = NULL;
+		root->right = NULL;
+		root->value = 0;
+
+		return root;
+	}
+
 	root = (Node*)malloc(sizeof(Node));
 	root->left = NULL;
 	root->right = NULL;
@@ -36,12 +46,12 @@ void foutFunction();
 void constructionTree();
 
 //*root, int *,int fNum1,int sNum1,int *,Num
-void preIn(int,int,int *,int *,Node *);
+Node* preIn(int,int,int *,int *,Node *);
 void prePost();
 void inPost();
 
 void preorderPrint();
-void inorderPrint();
+void inorderPrint(Node*);
 void postorderPrint();
 
 /*
@@ -104,7 +114,8 @@ void constructionTree()
 {
 	if( (orderType1 == -1 && orderType2 == 0) || (orderType1 == 0 && orderType2 == -1) )
 	{
-		preIn(finPre[0],1,finIn,finPre,root);
+//		root = insertNumber(root,finPre[0]);
+		root = preIn(finPre[0],0,finIn,finPre,root);
 	}
 	else if( (orderType1 == -1 && orderType2 == 1) || (orderType1 == 1 && orderType2 == -1) )
 	{}
@@ -114,13 +125,82 @@ void constructionTree()
 	return;
 }
 
-void preIn(int checkCenter,int checkPreNum,int *in,int *pre,Node *root)
+Node* preIn(int checkCenter,int checkPreNum,int *in,int *pre,Node *root)
 {
+//	root = insertNumber(root,checkCenter);
+
+	cout<<"inorderPrint"<<endl;
+	inorderPrint(root);
+	cout<<"end"<<endl;
+
 	if(root == NULL)
-		cout<<"NULL"<<endl;
+		cout<<"root is NULL "<<checkCenter<<" "<<checkPreNum<<endl;
+	else
+		cout<<root->value<<endl;
+	int tmpIn,tmpPre;
 	cout<<checkCenter<<" "<<checkPreNum<<" "<<in[3]<<" "<<pre[2]<<endl;
 
-	return;
+	for(int i = checkPreNum;i < nodeNum;i++)
+	{
+		if(in[i] == checkCenter)
+		{
+			tmpIn = i;
+			break;
+		}
+	}
+
+	for(int i = checkPreNum;i < nodeNum;i++)
+	{
+		if(checkPreNum == 0)
+		{
+			if(pre[i] == in[tmpIn])
+			{
+				tmpPre = i;
+				break;
+			}
+		}
+		else
+		{
+			if(pre[i] == in[tmpIn-1])
+			{
+				tmpPre = i;
+				break;
+			}
+		}
+	}
+
+	cout<<tmpIn<<" "<<in[tmpIn]<<" "<<tmpPre<<" "<<pre[tmpPre]<<endl;
+	
+	if(!(tmpIn+1 == tmpPre))
+	{
+		cout<<"In"<<endl;
+		if(root != NULL)
+			root = preIn(in[tmpIn-1],0,in,pre,root->left);
+		else if(root == NULL)
+		{
+			root = insertNumber(root,checkCenter);
+			root = preIn(in[tmpIn-1],0,in,pre,root->left);
+		}
+		cout<<"Out"<<endl;
+	}
+	inorderPrint(root);
+
+	cout<<"inserta "<<checkCenter<<" "<<endl;
+	root = insertNumber(root,checkCenter);
+	cout<<"insertb"<<endl;
+
+	inorderPrint(root);
+
+	return root;
+}
+
+void inorderPrint(Node* root)
+{
+	if(root == NULL)
+		return;
+	inorderPrint(root->left);
+	cout<<root->value<<" ";
+	inorderPrint(root->right);
 }
 
 int main()
@@ -128,6 +208,8 @@ int main()
 	finFunction();
 
 	constructionTree();
+
+//	inorderPrint(root);
 
 	fin.close();
 	fout.close();
