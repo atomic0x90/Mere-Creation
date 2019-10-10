eafd6f386f2679b7bfd43bca788221680e9d2e5d
atomic0x90 <yoojun20@gmail.com>
2019-10-10 17:27:07 +0900

    Add constructionTree function

--- algorithmProblem/treeConstructionByTraversals/tree2.cpp
+++ algorithmProblem/treeConstructionByTraversals/tree2.cpp
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+typedef struct NodeStruct
+{
+	int value;
+	struct NodeStruct* left;
+	struct NodeStruct* right;
+}Node;
+
+Node* root = NULL;
+
+Node* insertNumber(Node* root,int data)
+{
+	root = (Node*)malloc(sizeof(Node));
+	root->left = NULL;
+	root->right = NULL;
+	root->value = data;
+
+	return root;
+}
+
 ifstream fin("tree.inp");
 ofstream fout("tree.out");
 
@@ -16,19 +35,16 @@ void finFunction();
 void foutFunction();
 void constructionTree();
 
+//*root, int *,int fNum1,int sNum1,int *,Num
+void preIn(int,int,int *,int *,Node *);
+void prePost();
+void inPost();
+
 void preorderPrint();
 void inorderPrint();
 void postorderPrint();
 
-typedef struct NodeStruct
-{
-	int value;
-	struct NodeStruct* left;
-	struct NodeStruct* right;
-}Node;
-
-Node *root = NULL;
-
+/*
 Node* insertNumber(Node* root,int data)
 {
 	root = (Node*)malloc(sizeof(Node));
@@ -38,7 +54,7 @@ Node* insertNumber(Node* root,int data)
 
 	return root;
 }
-
+*/
 void finFunction()
 {
 	fin>>nodeNum;
@@ -62,12 +78,56 @@ void finFunction()
 			fin>>finPost[i];
 	}
 
+	fin>>orderType2;
+
+	cout<<orderType2<<endl;
+	if(orderType2 == -1)
+	{
+		for(int i = 0;i < nodeNum;i++)
+			fin>>finPre[i];
+	}
+	else if(orderType2 == 0)
+	{
+		for(int i = 0;i < nodeNum;i++)
+			fin>>finIn[i];
+	}
+	else if(orderType2 == 1)
+	{
+		for(int i = 0;i < nodeNum;i++)
+			fin>>finPost[i];
+	}
+
+	return;
+}
+
+void constructionTree()
+{
+	if( (orderType1 == -1 && orderType2 == 0) || (orderType1 == 0 && orderType2 == -1) )
+	{
+		preIn(finPre[0],1,finIn,finPre,root);
+	}
+	else if( (orderType1 == -1 && orderType2 == 1) || (orderType1 == 1 && orderType2 == -1) )
+	{}
+	else if( (orderType1 == 0 && orderType2 == 1) || (orderType1 == 1 && orderType2 == 0) )
+	{}
+
+	return;
+}
+
+void preIn(int checkCenter,int checkPreNum,int *in,int *pre,Node *root)
+{
+	if(root == NULL)
+		cout<<"NULL"<<endl;
+	cout<<checkCenter<<" "<<checkPreNum<<" "<<in[3]<<" "<<pre[2]<<endl;
+
 	return;
 }
 
 int main()
 {
 	finFunction();
+
+	constructionTree();
 
 	fin.close();
 	fout.close();
