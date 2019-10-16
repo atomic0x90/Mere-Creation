7cae97ed640080984295de93a10cfd2d392080ee
atomic0x90 <yoojun20@gmail.com>
2019-10-16 16:01:37 +0900

    Add foutFunction, init, checkInversionNumber

--- algorithmProblem/inversionNumber/inv.cpp
+++ algorithmProblem/inversionNumber/inv.cpp
@@ -1,19 +1,65 @@
 #include <iostream>
 #include <fstream>
 
+#include <time.h>
+
 using namespace std;
 
 ifstream fin("inv.inp");
 ofstream fout("inv.out");
 
+
 int checkInversion[100000] = {0,};
 int finNum[100000] = {0,};
 
-void checkInversionNumber();
+clock_t start;
+double end_t;
+
+void checkInversionNumber(int);
+void init(int);
+void foutFunction(int);
+
+void foutFunction(int ln)
+{
+	long long sum = 0;
+	for(int i = 0;i<ln;i++)
+		sum += checkInversion[i];
+
+	fout<<sum<<endl;
+
+	return;
+}
+
+void checkInversionNumber(int ln)
+{
+	for(int i = 0;i<ln;i++)
+	{
+		for(int j = i;j < ln;j++)
+		{
+			if(finNum[i] > finNum[j])
+				checkInversion[j]++;
+		}
+	}
+
+
+	return;
+}
+
+void init(int ln)
+{
+	for(int i = 0;i<ln;i++)
+	{
+		checkInversion[i] = 0;
+		finNum[i] = 0;
+	}
+	return;
+}
 
 int main()
 {
-	int testCase,ln,num;
+	start = clock();
+
+	int testCase,num,ln;
 
 	fin>>testCase;
 	while(testCase)
@@ -27,12 +73,19 @@ int main()
 		}
 		cout<<ln<<" ";
 
-	
+		checkInversionNumber(ln);
+
+		foutFunction(ln);
+
+		init(ln);
+
 		testCase--;
 	}
 
 	fin.close();
 	fout.close();
 
+	end_t = clock();
+	cout<<endl<<(double)(end_t-start)/CLOCKS_PER_SEC<<endl;
 	return 0;
 }
