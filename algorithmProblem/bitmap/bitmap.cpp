23b2247ed21a72bc7e18b489076d0bdf784f5b29
atomic0x90 <yoojun20@gmail.com>
2019-10-22 22:12:04 +0900

    Fix finFunction

--- algorithmProblem/bitmap/bitmap.cpp
+++ algorithmProblem/bitmap/bitmap.cpp
@@ -15,6 +15,7 @@ int rowSize,columnSize;
 
 char type;
 
+
 string finString;
 char *splitString;
 int checkL = 0;
@@ -45,35 +46,90 @@ void finFunction()
 {
 	while(1)
 	{
-		int tmp;
-
 		getline(fin,finString);
 		splitString = (char *)finString.c_str();
 		checkL = strlen(splitString);
 
 		if(splitString[0] == '#')
 			break;
-		else if(splitString[1] == ' ')
+		else
 		{
-			rowSize = 0,columnSize = 0;
-			for(int i = 2;;i++)
+			int tmp;
+			char finChar;
+			if(splitString[0] == 'B' && splitString[1] == ' ')
 			{
-				if(splitString[i] != ' ')
-					rowSize = rowSize*10 + atoi(splitString[i]);
-				else if(splitString[i] == ' ')
+				rowSize = columnSize = 0;
+				for(int i = 2;;i++)
 				{
-					tmp = i;
-					break;
+					if(splitString[i] == ' ')
+					{
+						tmp = i;
+						break;
+					}
+
+					rowSize  = rowSize * 10 + splitString[i] - '0';
+				}
+				for(int i = tmp + 1;i < checkL;i++)
+				{
+					columnSize = columnSize * 10 + splitString[i] - '0';
+				}
+				cout<<rowSize<<" "<<columnSize<<endl;
+
+				for(int i = 0;i<columnSize;i++)
+				{
+					for(int j = 0;j<rowSize;)
+					{
+						fin>>finChar;
+						if(finChar == '\n')
+							break;
+						else
+						{
+							dimB[i][j] = finChar;
+							j++;
+						}
+					}
 				}
 			}
-			for(int i = tmp+1;;i++)
+			else if(splitString[0] == 'D' && splitString[1] == ' ')
 			{
-				if(splitString[i] != ' ')
-					columnSize = columnSize*10 + atoi(splitString[i]);
-				else if(splitString[i] == '\n')
-					break;
-			}
+				rowSize = columnSize = 0;
+				for(int i = 2;;i++)
+				{
+					if(splitString[i] == ' ')
+					{
+						tmp = i;
+						break;
+					}
 
+					rowSize = rowSize * 10 + splitString[i] - '0';
+				}
+				for(int i = tmp + 1;i < checkL;i++)
+				{
+					columnSize = columnSize * 10 + splitString[i] - '0';
+				}
+				cout<<rowSize<<" "<<columnSize<<endl;
+
+				tmp = 0;
+
+				for(int i = 0;;)
+				{
+					getline(fin,finString);
+					splitString = (char *)finString.c_str();
+					checkL = strlen(splitString);
+				
+					for(int j = 0;j<checkL;j++)
+					{
+						dimD[i] = splitString[j];
+						cout<<dimD[i];
+						i++;
+					}
+
+					if(checkL < 50)
+						break;
+				}
+				cout<<endl;
+			
+			}
 		}
 	}
 	return;
