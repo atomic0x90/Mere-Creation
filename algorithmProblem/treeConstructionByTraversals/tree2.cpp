bb08c1c62fb6456328535f505f19d050ea7b6e47
atomic0x90 <yoojun20@gmail.com>
2019-10-27 20:08:30 +0900

    Fix preIn function

--- algorithmProblem/treeConstructionByTraversals/tree2.cpp
+++ algorithmProblem/treeConstructionByTraversals/tree2.cpp
@@ -12,9 +12,8 @@ typedef struct NodeStruct
 
 Node* root = NULL;
 
-Node* insertNumber(Node* root,int data)
+Node* insertNode(Node* root,int data)
 {
-
 	root = (Node*)malloc(sizeof(Node));
 	root->left = NULL;
 	root->right = NULL;
@@ -27,41 +26,19 @@ ifstream fin("tree.inp");
 ofstream fout("tree.out");
 
 int nodeNum;
+int maxPre = 0;
 int orderType1, orderType2;
 int finPre[10000] = {0,};
 int finIn[10000] = {0,};
 int finPost[10000] = {0,};
 
-int checkRoot;
-
-int maxPre = 0;
-int checkPre = 0;
+Node* preIn(Node*,int,int,int);
 
 void finFunction();
 void foutFunction();
 void constructionTree();
 
-
-//*root, int *,int fNum1,int sNum1,int *,Num
-Node* preIn(int,int,int,int *,int *,Node *,int);
-void prePost();
-void inPost();
-
-void preorderPrint();
-void inorderPrint(Node*);
-void postorderPrint();
-
-/*
-Node* insertNumber(Node* root,int data)
-{
-	root = (Node*)malloc(sizeof(Node));
-	root->left = NULL;
-	root->right = NULL;
-	root->value = data;
-
-	return root;
-}
-*/
+void postPrint(Node*);
 
 void finFunction()
 {
@@ -110,19 +87,19 @@ void finFunction()
 
 void constructionTree()
 {
+	int tmp;
 	if( (orderType1 == -1 && orderType2 == 0) || (orderType1 == 0 && orderType2 == -1) )
 	{
 		for(int i = 0;i<nodeNum;i++)
 		{
 			if(finPre[0] == finIn[i])
 			{
-				checkPre = i;
+				tmp = i;
 				break;
 			}
 		}
-		checkRoot = finPre[0];
-		maxPre = 0;
-		root = preIn(checkRoot,0,nodeNum,finIn,finPre,root,0);
+		//Node,left,middle,right,rrootI//,rrootP
+		root = preIn(root,0,tmp,nodeNum);
 	}
 	else if( (orderType1 == -1 && orderType2 == 1) || (orderType1 == 1 && orderType2 == -1) )
 	{}
@@ -132,124 +109,53 @@ void constructionTree()
 	return;
 }
 
-Node* preIn(int checkCenter,int checkF,int checkL,int *in,int *pre,Node *root,int rootData)
+Node* preIn(Node* root,int le,int mi,int ri)
 {
-	root = insertNumber(root,checkCenter);
+	root = insertNode(root,finIn[mi]);
 
-	cout<<"checkCenter "<<checkCenter<<" checkF "<<checkF<<" checkL "<<checkL;
-	cout<<" rootData "<<rootData<<" root->value "<<root->value<<endl;
-	if(checkF == checkL)
-		return root;
-
-	int tmpIn,tmpPre,tmpCenter = 0;
-	tmpIn = tmpPre = 0;
-
-	for(int i = checkF;i < checkL;i++)
+	int tmp;
+	if(mi - le > 0)		//left
 	{
-		if(in[i] == checkCenter)
+		maxPre++;
+		for(int i = 0;i<mi;i++)
 		{
-			tmpIn = i;
-			break;
-		}
-	}
-	for(int i = 0;i < nodeNum;i++)
-	{
-		if(pre[i] == in[tmpIn])
-		{
-			tmpPre = i + 1;
-			break;
-		}
-	}
-	for(int i = checkF;i < checkL;i++)
-	{
-		if(pre[tmpPre] == in[i])
-		{
-			tmpCenter = i;
-			break;
-		}
-	}
-
-	cout<<"tmpIn "<<in[tmpIn]<<" "<<tmpIn<<" tmpPre "<<pre[tmpPre]<<" ";
-	cout<<tmpPre<<" tmpCenter "<<in[tmpCenter]<<" "<<tmpCenter<<endl<<endl;
-
-	if(maxPre < tmpPre)
-		maxPre = tmpPre;
-
-	if(tmpCenter == 0)
-	{
-		root->left = insertNumber(root->left,in[tmpCenter]);
-	}
-/*	else if(tmpCenter == 0 && tmpIn != 1)
-	{
-		return root;
-	}
-*/	else
-	{
-		int checkMin = 0;
-		for(int i = 0;i<nodeNum;i++)
-		{
-			if(pre[tmpPre] == in[i])
+			if(finPre[maxPre] == finIn[i])
 			{
-				checkMin = i;
+				tmp = i;
 				break;
 			}
 		}
-		cout<<"checkMin "<<checkMin<<endl;	
-		if(checkMin < tmpIn)
-			root->left = preIn(in[tmpCenter],0,tmpIn,in,pre,root->left,checkCenter);
-		else
-			root->left = preIn(in[tmpCenter],checkMin,tmpIn,in,pre,root->left,checkCenter);
-		cout<<"outIn"<<endl<<endl;
+		root->left = preIn(root->left,le,tmp,mi);
 	}
-	inorderPrint(root);
-	cout<<endl<<"tmpIn "<<in[tmpIn]<<" "<<tmpIn<<" tmpPre "<<pre[tmpPre]<<" ";
-	cout<<tmpPre<<" tmpCenter "<<in[tmpCenter]<<" "<<tmpCenter<<endl;
-	int tmp = 0;
-	for(int i = 0;i<nodeNum;i++)
+
+	if(ri - mi > 1)		//right
 	{
-		if(in[tmpIn+1] == pre[i])
+		cout<<root->value<<" "<<le<<" "<<mi<<" "<<ri<<" "<<" "<<maxPre<<endl;
+	
+		for(int i = mi+1 ;i < ri ;i++)
 		{
-			tmp = i;
-			break;
-		}
-	}
-	cout<<"tmp "<<tmp<<" maxPre "<<maxPre<<endl<<endl;
-	if(tmp >= maxPre)
-	{
-		cout<<"right "<<maxPre;
-		cout<<endl<<"tmpIn "<<in[tmpIn]<<" "<<tmpIn<<" tmpPre "<<pre[tmpPre]<<" ";
-		cout<<tmpPre<<" tmpCenter "<<in[tmpCenter]<<" "<<tmpCenter<<endl;
-		cout<<"-----------"<<endl;
-		
-		int tmp1;
-		for(int i = 0;i<nodeNum;i++)
-		{
-			if(in[i] == rootData)
+			if(finIn[i] == finPre[maxPre+1])
 			{
-				tmp1 = i;
+				maxPre++;
+				tmp = i;
 				break;
 			}
 		}
-		if(checkPre != tmpIn && tmp != maxPre)
-			root->right = preIn(pre[maxPre+1],tmpIn+1,tmp1,in,pre,root->right,checkCenter);
-		else if(checkPre != tmpIn && tmp == maxPre)
-			root->right = preIn(pre[maxPre],tmpIn+1,tmp1,in,pre,root->right,checkCenter);
-		else
-			root->right = preIn(pre[maxPre],tmpIn+1,nodeNum,in,pre,root->right,checkCenter);
-	}	
-
-//	root->right = preIn(,,in,pre,root->right,checkCenter);
+		cout<<root->value<<" "<<le<<" "<<mi<<" "<<ri<<" "<<" "<<maxPre<<endl;
+		root->right = preIn(root->right,mi+1,tmp,ri);
+	}
 
 	return root;
 }
 
-void inorderPrint(Node* root)
+void postPrint(Node* root)
 {
 	if(root == NULL)
 		return;
-	inorderPrint(root->left);
+	postPrint(root->left);
+	postPrint(root->right);
 	cout<<root->value<<" ";
-	inorderPrint(root->right);
+	fout<<root->value<<endl;
 }
 
 int main()
@@ -259,7 +165,7 @@ int main()
 	constructionTree();
 
 	cout<<endl;
-	inorderPrint(root);
+	postPrint(root);
 
 	fin.close();
 	fout.close();
