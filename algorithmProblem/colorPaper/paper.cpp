b38d5bf9957cff05a9b9ef58de09be0e11a12a16
atomic0x90 <yoojun20@gmail.com>
2019-10-29 20:11:34 +0900

    Add overlapCheck function

--- algorithmProblem/colorPaper/paper.cpp
+++ algorithmProblem/colorPaper/paper.cpp
@@ -7,12 +7,13 @@ ifstream fin("paper.inp");
 ofstream fout("paper.out");
 
 int testCase;
-int x[2000] = {0,},y[2000] = {0,},xx[2000] = {0,},yy[2000] = {0,};
+int x[20000] = {0,},y[20000] = {0,},xx[20000] = {0,},yy[20000] = {0,};
 int checkOL[2000] = {0,};
 
 void finFunction();
 void overlapCheck();
 void foutFunction();
+void merge(int,int);
 
 void finFunction()
 {
@@ -38,81 +39,212 @@ void finFunction()
 
 void overlapCheck()
 {
-	int tmp = testCase-1;
+	int tmp1,tmp2;
+	tmp1 = tmp2 = testCase-1;
 
-	for(int i = tmp;i >= 0;i--)
+	for(int i = tmp1;i >= 0;i--)
 	{
-		for(int j = tmp;j > i;j--)
+		for(int j = tmp2;j > i;j--)
 		{
-			if(x[i] >= x[j] && y[i] >= y[j] && xx[i] <= xx[j] && yy[i] <= yy[j])
+			cout<<"tmp2 "<<tmp2<<endl;
+			if(x[j] <= x[i] && xx[j] >= xx[i] && y[j] <= y[i] && yy[j] >= yy[i])
 			{	//전부 덮일 경우
 				x[i] = x[j];
-				y[i] = y[j];
 				xx[i] = xx[j];
+				y[i] = y[j];
 				yy[i] = yy[j];
+
 				checkOL[i] = 2;
 				break;
 			}
+			//튀어나옴
+			else if(x[i] == x[j] && xx[i] == xx[j] && y[i] >= y[j] && y[i] <= yy[j] && yy[i] >= yy[j])
+			{	//x좌표 같고 y좌표 겹치는 경우(위로)
+				y[i] = y[j];
+				yy[j] = yy[i];
+
+				checkOL[i] = 1;
+			}
+			else if(x[i] == x[j] && xx[i] == xx[j] && yy[i] >= y[j] && yy[i] <= yy[j] && y[i] <= y[j])
+			{	//x좌표 같고 y좌표 겹치는 경우(아래로)
+				yy[i] = yy[j];
+				y[j] = y[i];
+
+				checkOL[i] = 1;
+			}
+			else if(y[i] == y[j] && yy[i] == yy[j] && x[i] <= x[j] && xx[i] >= x[j] && xx[i] <= xx[j])
+			{	//y좌표 같고 x좌표 겹치는 경우(왼쪽으로)
+				x[j] = x[i];
+				xx[i] = xx[j];
+
+				checkOL[i] = 1;
+			}
+			else if(y[i] == y[j] && yy[i] == yy[j] && xx[i] >= xx[j] && x[i] >= x[j] && x[i] <= xx[j])
+			{	//y좌표 같고 x좌표 겹치는 경우(오른쪽으로)
+				x[i] = x[j];
+				xx[j]=  xx[i];
+
+				checkOL[i] = 1;
+			}
+			//모서리 겹치는 경우
+			else if(x[i] <= x[j] && xx[i] >= x[j] && xx[i] <= xx[j] && yy[i] >= yy[j] && y[i] <= yy[j] && y[i] >= y[j])
+			{	//left top
+				tmp2++;
+				x[tmp2] = x[i];
+				xx[tmp2] = xx[j];
+				y[tmp2] = y[i];
+				yy[tmp2] = yy[j];
+
+				tmp2++;
+				x[tmp2] = x[j];
+				xx[tmp2] = xx[i];
+				y[tmp2] = y[j];
+				yy[tmp2] = yy[i];
+
+				checkOL[i] = 1;
+			}
+			else if(x[i] >= x[j] && x[i] <= xx[j] && xx[i] >= xx[j] && yy[i] >= yy[j] && y[i] <= yy[j] && y[i] >= y[j])
+			{	//right top
+				tmp2++;
+				x[tmp2] = x[j];
+				xx[tmp2] = xx[i];
+				y[tmp2] = y[i];
+				yy[tmp2] = yy[j];
+
+				tmp2++;
+				x[tmp2] = x[i];
+				xx[tmp2] = xx[j];
+				y[tmp2] = y[j];
+				yy[tmp2] = yy[i];
+
+				checkOL[i] = 1;
+			}
+			else if(x[i] <= x[j] && xx[i] >= x[j] && xx[i] <= xx[j] && y[i] <= y[j] && yy[i] >= y[j] && yy[i] <= yy[j])
+			{	//left bottom
+				tmp2++;
+				x[tmp2] = x[i];
+				xx[tmp2] = xx[j];
+				y[tmp2] = y[j];
+				yy[tmp2] = yy[i];
+
+				tmp2++;
+				x[tmp2] = x[j];
+				xx[tmp2] = xx[i];
+				y[tmp2] = y[i];
+				yy[tmp2] = yy[j];
+				
+				checkOL[i] = 1;
+			}
+			else if(x[i] >= x[j] && x[i] <= xx[j] && xx[i] >= xx[j] && y[i] <= y[j] && yy[i] >= y[j] && yy[i] <= yy[j])
+			{	//right bottom
+				tmp2++;
+				x[tmp2] = x[j];
+				xx[tmp2] = xx[i];
+				y[tmp2] = y[j];
+				yy[tmp2] = yy[i];
+
+				tmp2++;
+				x[tmp2] = x[i];
+				xx[tmp2] = xx[j];
+				y[tmp2] = y[i];
+				yy[tmp2] = yy[j];
+
+				checkOL[i] = 1;
+			}
+			//i 종이가 더 작은 경우
+			else if(y[i] >= y[j] && yy[i] <= yy[j] && x[i] < x[j] && xx[i] >= x[j] && xx[i] <= xx[j])
+			{	//왼쪽으로 튀어나온 경우
+				xx[i] = xx[j];
+
+				checkOL[i] = 1;
+			}
+			else if(y[i] >= y[j] && yy[i] <= yy[j] && xx[i] > xx[j] && x[i] >= x[j] && x[i] <= xx[j])
+			{	//오른쪽으로 튀어나온 경우
+				x[i] = x[j];
+
+				checkOL[i] = 1;
+			}
+			else if(x[i] >= x[j] && xx[i] <= xx[j] && yy[i] > yy[j] && y[i] >= y[j] && y[i] <= yy[j])
+			{	//위로 튀어나온 경우
+				y[i] = y[j];
+
+				checkOL[i] = 1;
+			}
+			else if(x[i] >= x[j] && xx[i] <= xx[j] && y[i] < y[j] && yy[i] <= yy[j] && yy[i] >= y[j])
+			{	//아래로 튀어나온 경우
+				yy[i] = yy[j];
+
+				checkOL[i] = 1;
 			
-			if(x[i] == x[j] && xx[i] == xx[j])
-			{	//x좌표가 같을 경우
-				if(y[i] <= y[j] && y[j] <= yy[i] && yy[i] <= yy[j])
-				{
-					y[j] = y[i];
-					yy[i] = yy[j];
-					checkOL[i] = 1;
-				}
-				else if(y[i] >= y[j] && y[i] <= yy[j] && yy[i] >= yy[j])
-				{
-					y[i] = y[j];
-					yy[j] = yy[i];
-					checkOL[i] = 1;
-				}
 			}
-			else if(y[i] == y[j] && yy[i] == yy[j])
-			{	//y좌표가 같을 경우
-				if(x[i] <= x[j] && x[j] <= xx[i] && xx[i] <= xx[j])
-				{
-					x[j] = x[i];
-					xx[i] = xx[j];
-					checkOL[i] = 1;
-				}
-				else if(x[i] >= x[j] && x[i] <= xx[j] && xx[i] >= xx[j])
-				{
-					x[i] = x[j];
-					xx[j] = xx[i];
-					checkOL[i] = 1;
-				}
-			}
-			else if( ((xx[i] < x[j]) || (x[i] > xx[j])) && ((yy[i] < y[j]) || (y[i] > yy[j])) )
-			{	//겹치지 않는 경우
-//				if(checkOL[i] != 1)
-//					checkOL[i] = 0;
-			}
-			else if( (x[i] <= x[j] && xx[i] >= x[j]) || (x[i] <= xx[j] && xx[i] >= xx[j]) )
-			{
-				//모서리
-				if( (y[i] <= y[j] && yy[i] >= y[j]) || (y[i] <= yy[j] && yy[i] >= yy[j]) )
-					checkOL[i] = 1;
-				else if(y[i] >= y[j] && yy[i] <= yy[j])
-					checkOL[i] = 1;
-			}
-			else if( (y[i] <= y[j] && yy[i] >= y[j]) || (y[i] <= yy[j] && yy[i] >= yy[j]) )
-			{
-				//모서리
-				if( (x[i] <= x[j] && xx[i] >= x[j]) || (x[i] <= xx[j] && xx[i] >= xx[j]) )
-					checkOL[i] = 1;
-				else if(x[i] >= x[j] && xx[i] <= xx[j])
-					checkOL[i] = 1;
-			}
+			//i 종이가 더 큰 경우
+			else if(x[i] >= x[j] && x[i] <= xx[j] && xx[i] >= xx[j] && y[i] <= y[j] && yy[i] >= yy[j])
+			{	//i 종이 왼쪽에 튀어나온 경우
+				xx[j] = xx[i];
 
-//			else
-//				checkOL[i] = 1;
+				checkOL[i] = 1;
+			}
+			else if(x[i] >= x[j] && x[i] <= xx[j] && xx[i] >= xx[j] && y[i] <= y[j] && yy[i] >= yy[j])
+			{	//i 종이 오른쪽에 튀어나온 경우
+				x[j] = x[i];
 
+				checkOL[i] = 1;
+			}
+			else if(y[i] >= y[j] && y[i] <= yy[j] && yy[i] >= yy[j] && x[i] >= x[j] && xx[i] <= xx[j])
+			{	//i 종이 아래쪽에 튀어나온 경우
+				yy[j] = yy[i];
+
+				checkOL[i] = 1;
+			}
+			else if(y[i] <= y[j] && yy[i] >= y[j] && yy[i] <= yy[j] && x[i] >= x[j] && xx[i] <= xx[j])
+			{	//i 종이 위쪽에 튀어나온 경우
+				y[j] = y[i];
+
+				checkOL[i] = 1;
+			}
+			else if(y[i] >= y[j] && yy[i] <= yy[j] && x[i] <= x[j] && xx[i] >= xx[j])
+			{	//가로로 통과한 경우
+				checkOL[i] = 1;
+			}
+			else if(x[i] >= x[j] && xx[i] <= xx[j] && y[i] <= y[j] && yy[i] >= yy[j])
+			{	//세로로 통과한 경우
+				checkOL[i] = 1;
+			}
 		}
+		//merge(i,tmp2);
 	}
 
 	return;
+}
+
+void merge(int s,int l)
+{
+	for(int i = s;i <= l;i++)
+	{
+		for(int j = i;j <= l;j++)
+		{
+			if(x[i] == x[j] && xx[i] == xx[j] && y[i] <= y[j] && yy[i] >= y[j] && yy[i] <= yy[j])
+			{	//x좌표 같고 아래로
+				y[j] = y[i];
+				yy[i] = yy[j];
+			}
+			else if(x[i] == x[j] && xx[i] == xx[j] && y[i] >= y[j] && y[i] <= yy[j] && yy[i] >= yy[j])
+			{	//x좌표 같고 위로
+				y[i] = y[j];
+				yy[j] = yy[i];
+			}
+			else if(y[i] == y[j] && yy[i] == yy[j] && x[i] <= x[j] && xx[i] >= x[j] && xx[i] <= xx[j])
+			{	//y좌표 같고 왼쪽으로
+				x[j] = x[i];
+				xx[i] = xx[j];
+			}
+			else if(y[i] == y[j] && yy[i] == yy[j] && x[i] >= x[j] && x[i] <= xx[j] && xx[i] >= xx[j])
+			{	//y좌표 같고 오른쪽으로
+				x[i] = x[j];
+				xx[j] = xx[i];
+			}
+		}
+	}
 }
 
 void foutFunction()
@@ -144,5 +276,7 @@ int main()
 	}
 	foutFunction();
 
+	fin.close();
+	fout.close();
 	return 0;
 }
