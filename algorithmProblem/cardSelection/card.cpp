7746da69640fe1d8ff3c43be48e86ebe93306c68
atomic0x90 <yoojun20@gmail.com>
2019-12-16 17:21:35 +0900

    Test card algo

--- algorithmProblem/cardSelection/card.cpp
+++ algorithmProblem/cardSelection/card.cpp
@@ -43,6 +43,7 @@ void finFunction()
 	return;
 }
 
+
 void card()
 {
 	int tmp = cardLength;
@@ -52,6 +53,8 @@ void card()
 
 	int checkl = tmp;	//Check last card;
 
+	int tmpj = 2;
+
 	for(int i = cardLength;i > 0;)
 	{
 		t2 = t3 = t4 = -10000;
@@ -60,8 +63,14 @@ void card()
 		c3 = i - 3;
 		c2 = i - 2;
 		
-		cout<<"i "<<i<<" "<<cardLine[i]<<" "<<checkl<<endl;
-		
+		cout<<"i "<<i<<" "<<cardLine[i]<<" "<<endl;
+/*		if(c4 > 3)
+			cout<<cardLine[c4]<<" "<<cardLine[c3]<<" "<<cardLine[c2]<<endl;
+		else if(c3 > 3)
+			cout<<cardLine[c3]<<" "<<cardLine[c2]<<endl;
+		else if(c2 > 3)
+			cout<<cardLine[c2]<<endl;
+*/
 		if(i > 3)
 		{
 			result += cardLine[i];
@@ -70,43 +79,225 @@ void card()
 				t4 = cardLine[c4];
 			else if(c4 > 3)
 			{
-				for(int j = 2;j < 5;j++)
+				for(int j = tmpj;j < 5;j++)
 				{
-					if(cardLine[c4] + cardLine[c4 - j] > t4)
-						t4 = cardLine[c4] + cardLine[c4 - j];
+					if(c4 - j <= 3 && c4 - j > 0)
+					{
+						if(cardLine[c4] + cardLine[c4-j] > t4)
+							t4 = cardLine[c4] + cardLine[c4-j];
+					}
+					else if(c4 - j > 3)
+					{
+						int k;
+						if(j == 2)
+							k = 3;
+						else
+							k = 2;
+	
+						for(;k < 5;k++)
+						{
+							if(c4 - j - k <= 3 && c4 - j - k > 0)
+                               	        		{
+                               	        	        	if(cardLine[c4] + cardLine[c4-j] + cardLine[c4-j-k] > t4)
+                               	                         		t4 = cardLine[c4] + cardLine[c4-j] + cardLine[c4-j-k];
+                               	     	 		}
+							else if(c4 - j - k > 3)
+							{
+								int l;
+								if(k == 2)
+									l = 3;
+								else
+									l = 2;
+								
+								for(;l < 5;l++)
+								{
+									if(c4 - j - k - l <= 3 && c4 - j - k - l > 0)
+									{
+										if(cardLine[c4] + cardLine[c4-j] + cardLine[c4-j-k] + cardLine[c4-j-k-l] > t4)
+                                       	                    	  		   	t4 = cardLine[c4] + cardLine[c4-j] + cardLine[c4-j-k] + cardLine[c4-j-k-l];
+									}
+									else if(c4 - j - k - l > 3)
+									{
+										int o;
+										if(l == 2)
+											o = 3;
+										else
+											o = 2;
+
+										for(;o < 5;o++)
+										{
+											if(c4 - j - k - l - o > 0)
+											{
+												if(cardLine[c4] + cardLine[c4-j] + cardLine[c4-j-k] + cardLine[c4-j-k-l] + cardLine[c4-j-k-l-o] > t4)
+                                                                                        		t4 = cardLine[c4] + cardLine[c4-j] + cardLine[c4-j-k] + cardLine[c4-j-k-l] + cardLine[c4-j-k-l-o];
+										
+											}
+										}
+									}
+								}
+							}
+						}
+					}
 				}
 			}
-			
 			if(c3 <= 3 && c3 > 0)
 				t3 = cardLine[c3];
 			else if(c3 > 3)
 			{
-				for(int j = 2;j < 5;j++)
-				{
-					if(cardLine[c3] + cardLine[c3 - j] > t3)
-						t3 = cardLine[c3] + cardLine[c3 - j];
-				}
+				for(int j = tmpj;j < 5;j++)
+                                {
+                                        if(c3 - j <= 3 && c3 - j > 0)
+                                        {
+                                                if(cardLine[c3] + cardLine[c3-j] > t3)
+                                                        t3 = cardLine[c3] + cardLine[c3-j];
+                                        }
+                                        else if(c3 - j > 3)
+                                        {
+                                                int k;
+                                                if(j == 2)
+                                                        k = 3;
+                                                else
+                                                        k = 2;
+
+                                                for(;k < 5;k++)
+                                                {
+                                                        if(c3 - j - k <= 3 && c3 - j - k > 0)
+                                                        {
+                                                                if(cardLine[c3] + cardLine[c3-j] + cardLine[c3-j-k] > t3)
+                                                                	t3 = cardLine[c3] + cardLine[c3-j] + cardLine[c3-j-k];
+                                                        }
+                                                        else if(c3 - j - k > 3)
+                                                        {
+                                                                int l;
+                                                                if(k == 2)
+                                                                        l = 3;
+                                                                else
+                                                                        l = 2;
+
+                                                                for(;l < 5;l++)
+                                                                {
+									if(c3 - j - k - l <= 3 && c3 - j - k - l > 0)
+                                                                        {
+                                                                                if(cardLine[c3] + cardLine[c3-j] + cardLine[c3-j-k] + cardLine[c3-j-k-l] > t3)
+                                                                                        t3 = cardLine[c3] + cardLine[c3-j] + cardLine[c3-j-k] + cardLine[c3-j-k-l];
+                                                                        }
+                                                                        else if(c3 - j - k - l > 3)
+                                                                        {
+                                                                                int o;
+                                                                                if(l == 2)
+                                                                                        o = 3;
+                                                                                else
+                                                                                        o = 2;
+
+                                                                                for(;o < 5;o++)
+                                                                                {
+											if(c3 - j - k - l - o > 0)
+											{
+                                                                                       		if(cardLine[c3] + cardLine[c3-j] + cardLine[c3-j-k] + cardLine[c3-j-k-l] + cardLine[c3-j-k-l-o] > t3)
+                                                                                       		        t3 = cardLine[c3] + cardLine[c3-j] + cardLine[c3-j-k] + cardLine[c3-j-k-l] + cardLine[c3-j-k-l-o];
+                                                                                	}
+										}
+                                                                        }
+                                                                }
+                                                        }
+                                                }
+                                        }
+                                }
 			}
 			
+			if(tmpj == 2)
+			{
 			if(c2 <= 3 && c2 > 0)
 				t2 = cardLine[c2];
 			else if(c2 > 3)
 			{
 				for(int j = 3;j < 5;j++)
-				{
-					if(cardLine[c2] + cardLine[c2 - j] > t2)
-						t2 = cardLine[c2] + cardLine[c2 - j];
-				}
+                                {
+                                        if(c2 - j <= 3 && c2 - j > 0)
+                                        {
+                                                if(cardLine[c2] + cardLine[c2-j] > t2)
+						{
+                                                        t2 = cardLine[c2] + cardLine[c2-j];
+//						cout<<"t2 "<<t2<<"j "<<j<<endl;
+						}
+                                        }
+                                        else if(c2 - j > 3)
+                                        {
+                                                int k = 2;
+
+                                                for(;k < 5;k++)
+                                                {
+                                                        if(c2 - j - k <= 3 && c2 - j - k > 0)
+                                                        {
+                                                                if(cardLine[c2] + cardLine[c2-j] + cardLine[c2-j-k] > t2)
+								{
+									t2 = cardLine[c2] + cardLine[c2-j] + cardLine[c2-j-k];
+//								cout<<"t2 "<<t2<<"j k "<<j<<" "<<k<<endl;
+								}
+                                                        }
+                                                        else if(c2 - j - k > 3)
+                                                        {
+                                                                int l;
+                                                                if(k == 2)
+                                                                        l = 3;
+                                                                else
+                                                                        l = 2;
+
+                                                                for(;l < 5;l++)
+                                                                {
+									if(c2 - j - k - l <= 3 && c2 - j - k - l > 0)
+                                                                        {
+                                                                                if(cardLine[c2] + cardLine[c2-j] + cardLine[c2-j-k] + cardLine[c2-j-k-l] > t2)
+										{        
+											t2 = cardLine[c2] + cardLine[c2-j] + cardLine[c2-j-k] + cardLine[c2-j-k-l];
+  //                                                                      	cout<<"t2 "<<t2<<" j k l "<<j<<" "<<k<<" "<<l<<"\t"<<cardLine[c2]<<" "<<cardLine[c2-j]<<" "<<cardLine[c2-j-k]<<" "<<cardLine[c2-j-k-l]<<endl;
+										}
+									}
+                                                                        else if(c2 - j - k - l > 3)
+                                                                        {
+                                                                                int o;
+                                                                                if(l == 2)
+                                                                                        o = 3;
+                                                                                else
+                                                                                        o = 2;
+
+                                                                                for(;o < 5;o++)
+                                                                                {
+											if(c2 - j - k - l - o > 0)
+											{
+                                                                                       		if(cardLine[c2] + cardLine[c2-j] + cardLine[c2-j-k] + cardLine[c2-j-k-l] + cardLine[c2-j-k-l-o] > t2)
+												{
+													t2 = cardLine[c2] + cardLine[c2-j] + cardLine[c2-j-k] + cardLine[c2-j-k-l] + cardLine[c2-j-k-l-o];
+    //                                                                            		cout<<"t2 "<<t2<<" j k l o"<<j<<" "<<k<<" "<<l<<" "<<o<<"\t"<<cardLine[c2]<<" "<<cardLine[c2-j]<<" "<<cardLine[c2-j-k]<<" "<<cardLine[c2-j-k-l]<<" "<<cardLine[c2-j-k-l-o]<<endl;
+												}
+											}
+										}
+                                                                        }
+                                                                }
+                                                        }
+                                                }
+                                        }
+                                }
+			}
 			}
 		
 			checkl = i;
 
 			if(t4 > t3 && t4 > t2)	//t4
+			{
 				i = c4;
+				tmpj = 2;
+			}
 			else if(t3 > t4 && t3 > t2)	//t3
+			{
 				i = c3;
+				tmpj = 2;
+			}
 			else if(t2 > t4 && t2 > t3)	//t2
+			{
 				i = c2;
+				tmpj = 3;
+			}
 		
 			cout<<"t data "<<t4<<" "<<t3<<" "<<t2<<endl;
 		}
