eec9564527c3a5e125633688cb0a354d5a172af3
atomic0x90 <yoojun20@gmail.com>
2019-12-20 20:19:29 +0900

    Test algo

--- algorithmProblem/cardSelection/card.cpp
+++ algorithmProblem/cardSelection/card.cpp
@@ -52,176 +52,205 @@ void finFunction()
 
 void card()
 {
-	for(int i = 1;i <= cardLength;i++)
-	{
-		if(i <= 3 || i > cardLength - 2)
-		{
-			tmpl[i] = cardLine[i];
-		}
-		else if(i == cardLength - 2)
-		{
-			for(int j = 1;j <= 3;j++)
-				tmpl[i] += cardLine[i-j];
+	return;
+}
 
-			for(int j = 1;j <= 2;j++)
-				tmpl[i] += cardLine[i+j];
+/*
+ * tmpl == 0 : Not decision
+ * tmpl == 1 : Selected
+ * tmpl == 2 : Not selected
+ *
+*/
+void algo()
+{
+	if(cardLength <= 5)
+	{
+	
+		return;
+	}
+
+	for(int i = 0;i <= cardLength - 2;)
+	{
+		cout<<"i "<<i<<endl;
+		int se = 0;
+		int checkl;
+		int tlen = cardLength - i;
+		if(i == 0)
+		{
+			for(int j = 1;j <= 4;j++)
+			{
+				if(se < cardLine[i+j])
+				{
+					se = cardLine[i+j];
+					checkl = j;
+				}
+			}
+			
+			result += se;
+			tmpl[i+checkl] = 1;
+			tmpl[i+checkl-1] = tmpl[i+checkl+1] = 2;
+			i += checkl;
+
+			if(checkl == 1)
+			{
+				tmpl[3] = 2;
+			}
+			else if(checkl == 3)
+			{
+				tmpl[1] = 2;
+			}
 		}
 		else
 		{
-			for(int j = 1;j <= 3;j++)
-				tmpl[i] += cardLine[i-j];
+			if(i == 1)
+			{
+				if(cardLength - i < 6)
+				{
+					result += cardLine[i+3] + cardLine[i+5];
+					return;
+				}
+				else
+				{
+					for(int j = 3;j < 5;j++)
+					{
+						if(se < cardLine[i+j])
+						{
+							se = cardLine[i+j];
+							checkl = j;
+						}
+					}
 
-			for(int j = 1;j <= 3;j++)
-				tmpl[i] += cardLine[i+j];
+					result += se;
+					tmpl[i+checkl] = 1;
+					tmpl[i+checkl-1] = tmpl[i+checkl+1] = 2;
+					i += checkl;
+				}
+			}
+			else if(i == 2)
+			{
+				if(cardLength - i < 6)
+				{
+
+					return;
+				}
+				else
+				{
+					for(int j = 2;j < 5;j++)
+					{
+						if(se < cardLine[i+j])
+						{
+							se = cardLine[i+j];
+							checkl = j;
+						}
+					}
+
+					result += se;
+					tmpl[i+checkl] = 1;
+					tmpl[i+checkl-1] = tmpl[i+checkl+1] = 2;
+					i += checkl;
+				}
+			}
+			else if(i == 3)
+			{
+				if(cardLength - i < 6)
+				{
+					return;
+				}
+				else
+				{
+					for(int j = 2;j < 5;j++)
+					{
+						if(se < cardLine[i+j])
+						{
+							se = cardLine[i+j];
+							checkl = j;
+						}
+					}
+
+					result += se;
+					tmpl[i+checkl] = 1;
+					tmpl[i+checkl-1] = tmpl[i+checkl+1] = 2;
+					i += checkl;
+				}
+			}
+			else
+			{
+				if(i == 4)
+				{
+					if(cardLine[1] > cardLine[2])
+					{
+						tmpl[1] = 1;
+						result += cardLine[1];
+					}
+					else
+					{
+						tmpl[2] = 1;
+						result += cardLine[2];
+					}
+				}
+
+				if(cardLength - i < 6)
+				{
+					if(tlen == 5)
+					{
+						if(tmpl[i-2] == 1)
+						{
+							result += cardLine[i+3] + cardLine[i+5];
+						}
+						else
+						{
+							if(cardLine[i+2] > cardLine[i+3])
+								result += cardLine[i+2] + cardLine[i+5];
+							else
+								result += cardLine[i+3] + cardLine[i+5];
+						}
+					}
+					else if(tlen == 4)
+					{
+						result += cardLine[i+4];
+					}
+					else if(tlen == 3)
+					{
+						result += cardLine[i+3];
+					}
+					else if(tlen == 2)
+					{
+						result += cardLine[i] + cardLine[i+2];
+					}
+					break;
+				}
+				else
+				{
+					int tmpj;
+					if(tmpl[i-2] == 1)
+						tmpj = 3;
+					else
+						tmpj = 2;
+
+					for(int j = tmpj;j < 5;j++)
+					{
+						if(se < cardLine[i+j])
+						{
+							se = cardLine[i+j];
+							checkl = j;
+						}
+					}
+
+					result += se;
+					tmpl[i+checkl] = 1;
+					tmpl[i+checkl-1] = tmpl[i+checkl+1] = 2;
+					i += checkl;
+				}
+			}
 		}
 	}
 
 	return;
 }
 
-void algo()
-{
-	int range = 0;
-
-	for(int i = cardLength;i > 0;)
-	{
-		int low = 10000;
-		int check = 0;
-		cout<<"i "<<i<<endl;
-		if(i == cardLength)
-		{
-			cout<<"First "<<cardLine[i]<<endl;
-			result += cardLine[i];
-
-			i -= 2;
-
-			range = 2;
-		}
-		else if(i < 7)
-		{
-			cout<<"in < 7 "<<i<<" "<<range<<endl;
-			if(i == 2)
-			{
-				if(tmpl[1] > tmpl[2])
-					result += tmpl[1];
-				else
-					result += tmpl[2];
-			
-				break;
-			}
-			else if(i == 3)
-			{
-				if(range == 2)	//1,2,3
-				{
-					if(tmpl[1] > tmpl[2] && tmpl[1] > tmpl[3])
-						result += tmpl[1];
-					else if(tmpl[2] > tmpl[1] && tmpl[2] > tmpl[3])
-						result += tmpl[2];
-					else if(tmpl[3] > tmpl[1] && tmpl[3] > tmpl[2])
-						result += tmpl[3];
-				}
-				else if(range == 1)	//2,3
-				{
-					if(tmpl[1] > tmpl[2])
-						result += tmpl[1];
-					else
-						result += tmpl[2];
-				}
-			
-				break;
-			}
-			for(int j = 4;j < 7;j++)
-			{
-				if(low > tmpl[j])
-				{
-					check = j;
-					low = tmpl[j];
-				}
-			}
-
-			if(check == 4)
-			{
-				result += cardLine[4];
-
-				if(tmpl[1] > tmpl[2])
-					result += cardLine[1];
-				else
-					result += cardLine[2];
-			}
-			else if(check == 5)
-			{
-				result += cardLine[5];
-
-				if(tmpl[1] > tmpl[2] && tmpl[1] > tmpl[3])
-					result += tmpl[1];
-				else if(tmpl[2] > tmpl[1] && tmpl[2] > tmpl[3])
-					result += tmpl[2];
-				else if(tmpl[3] > tmpl[1] && tmpl[3] > tmpl[2])
-					result += tmpl[3];
-			}
-			else if(check == 6)
-			{
-				result += cardLine[6];
-
-				if(cardLine[4] + cardLine[1] > cardLine[3] && cardLine[4] + cardLine[1] > cardLine[2])
-				{
-					result += cardLine[4] + cardLine[1];
-				}
-				else if(cardLine[3] > cardLine[4] + cardLine[1] && cardLine[3] > cardLine[2])
-				{
-					result += cardLine[3];
-				}
-				else if(cardLine[2] > cardLine[4] + cardLine[1] && cardLine[2] > cardLine[3])
-				{
-					result += cardLine[2];
-				}
-			}
-
-
-			break;
-		}
-		else
-		{
-			cout<<"cardLine "<<cardLine[i]<<" "<<cardLine[i-1]<<" "<<cardLine[i-2]<<endl;
-			cout<<"tmp "<<result<<"\t\t"<<tmpl[i]<<" "<<tmpl[i-1]<<" "<<tmpl[i-2]<<endl;
-			for(int j = i;j >= i-range;j--)
-			{
-				if(low > tmpl[j])
-				{
-					check = j;
-					low = tmpl[j];
-				}
-			}
-
-			if(check == i)
-			{
-				result += cardLine[check];
-				i -= 3;
-
-				range = 1;
-			}
-			else if(check == i - 1)
-			{
-				result += cardLine[check];
-				i -= 3;
-
-				range = 2;
-			}
-			else if(check == i - 2)
-			{
-				result += cardLine[check];
-				i -= 4;
-
-				range = 2;
-			}
-			cout<<"select i "<<check<<endl<<endl;
-		}
-	}
-}
-
 void init()
 {
-	for(int i = 0;i < 1001;i++)
+	for(int i = 1;i <= 1000;i++)
 		tmpl[i] = 0;
 
 	result = 0;
