2916caa73312e1fbfabac22303f239ea4f887b7f
atomic0x90 <yoojun20@gmail.com>
2019-12-23 23:54:56 +0900

    Add algo function(score 100)

--- algorithmProblem/poleArrangement/pole.cpp
+++ algorithmProblem/poleArrangement/pole.cpp
@@ -11,17 +11,65 @@ ofstream fout("pole.out");
 clock_t start_t;
 double end_t;
 
-long long pole[21][21][21];
+long long pole[21][21][21] = {0,};
 
-void init();
+int testCase;
+int n,l,r;
+
 void finFunction();
 void foutFunction();
 void algo();
+
+void finFunction()
+{
+	fin>>n>>l>>r;
+
+	return;
+}
+
+void foutFunction()
+{
+	cout<<pole[n][l][r]<<endl;
+	fout<<pole[n][l][r]<<endl;
+
+	return;
+}
+
+void algo()
+{
+	pole[1][1][1] = 1;
+
+	for(int i = 2;i <= 20;i++)
+	{
+		for(int j = 1;j <= 20;j++)
+		{
+			for(int k = 1;k <= 20;k++)
+			{
+				pole[i][j][k] = pole[i-1][j-1][k] + pole[i-1][j][k-1] + ( (i-2) * pole[i-1][j][k] );
+			}
+		}
+	}
+
+	return;
+}
 
 int main()
 {
 	start_t = clock();
 
+	algo();
+
+	fin>>testCase;
+	
+	while(testCase)
+	{
+		finFunction();
+
+
+		foutFunction();
+
+		testCase--;
+	}
 
 
 	fin.close();
