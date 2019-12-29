25c35dd7dfe5e97971266fc4f117075cab92279e
atomic0x90 <yoojun20@gmail.com>
2019-12-29 11:53:32 +0900

    Fix algo function(score 100)

--- algorithmProblem/coinGame/coin.cpp
+++ algorithmProblem/coinGame/coin.cpp
@@ -11,18 +11,118 @@ ofstream fout("coin.out");
 clock_t start_t;
 double end_t;
 
+int b1,b2,b3;
+
 int testCase;
-int coin1[1001],coin2[1001],coin3[1001];
+int result;
 
 void init();
 void finFunction();
 void foutFunction();
 int algo(int,int,int);
 
+void finFunction()
+{
+	fin>>b1>>b2>>b3;
+
+	return;
+}
+
+void init()
+{
+	result = 0;
+	return;
+}
+
+void foutFunction()
+{
+//	cout<<"("<<b1<<" "<<b2<<" "<<b3<<") : "<<result<<endl;
+	fout<<"("<<b1<<" "<<b2<<" "<<b3<<") : "<<result<<endl;
+	
+	return;
+}
+
+int algo(int t1,int t2,int t3)
+{
+	int max,data;
+
+	t1 %= 4;
+	t2 %= 4;
+	t3 %= 4;
+	
+//	cout<<t1<<" "<<t2<<" "<<t3<<endl;
+
+	if(t1 >= t2 && t1 >= t3)
+		max = t1;
+	else if(t2 >= t1 && t2 >= t3)
+		max = t2;
+	else
+		max = t3;
+
+	if( (t1 == 0 && t2 != 0 && t3 != 0) || (t1 != 0 && t2 == 0 && t3 != 0) || (t1 != 0 && t2 != 0 && t3 == 0) )
+	{	//1개 0
+		if(t1+t2+t3 == 2)
+			return 1;
+
+		data = (t1+t2+t3) % max;
+
+		if(data == 0)
+			return -1;
+		else
+			return 1;
+	}
+	else if( (t1 == 0 && t2 == 0 && t3 != 0) || (t1 == 0 && t2 != 0 && t3 == 0) || (t1 != 0 && t2 == 0 && t3 == 0) )
+	{	//2개 0
+		if(t1 + t2 + t3 == 1)
+			return -1;
+		else
+			return 1;
+	}
+	else if(t1 == 0 && t2 == 0 && t3 == 0)
+	{	//3개 0
+		return 1;
+	}
+	else
+	{
+		if(t1+t2+t3 == 3)
+			return -1;
+		else
+		{
+			if(max == 2 || t1 + t2 + t3 == 9)
+			{
+				return 1;
+			}
+			else
+			{
+				data = (t1+t2+t3) % max;
+
+				if(data == 0)
+					return -1;
+				else
+					return 1;
+			}
+		}
+	}
+}
+
+
 int main()
 {
 	start_t = clock();
 
+	fin>>testCase;
+	while(testCase)
+	{
+		finFunction();
+		
+		init();
+		
+		result = algo(b1,b2,b3);
+
+		foutFunction();
+	
+		testCase--;
+	}
 
 	fin.close();
 	fout.close();
