5225f15190222cafda1cbe422bf49dac711bd556
atomic0x90 <yoojun20@gmail.com>
2020-01-23 19:37:30 +0900

    Add kruskal algo

--- algorithmProblem/MST_AlgorithmsImplementation/mst.cpp
+++ algorithmProblem/MST_AlgorithmsImplementation/mst.cpp
@@ -14,45 +14,81 @@ using namespace std;
 ifstream fin("mst.inp");
 ofstream fout("mst.out");
 
-typedef tuple <int,int,int> tu;	// weighted of edge, edge number, v
+typedef tuple <int,int,int,int> tu;	// weighted of edge, edge number, v1, v2
 
 clock_t start_t;
 double end_t;
 
-vector < vector <tu> > v;
+vector < tu > v;
+
+vector <int> save;
 
 int visit[10001];
 
 int vertex,edge;
+int sum;
 
 void finFunction();
 int prim();
-int kruskal();
+void kruskal();
 void init();
 void sortFunction();
 
+int find(int);
+void unionParent(int,int);
+bool checkParent(int,int);
+
+int find(int x)
+{
+	if(visit[x] == x)
+		return x;
+
+	return visit[x] = find(visit[x]);
+}
+
+void unionParent(int x,int y)
+{
+	int p1,p2;
+
+	p1 = find(x);
+	p2 = find(y);
+
+	if(p1 < p2)
+		visit[y] = p1;
+	else
+		visit[x] = p2;
+
+	return;
+}
+
+bool checkParent(int x,int y)
+{
+	int p1,p2;
+
+	p1 = find(x);
+	p2 = find(y);
+
+	if(p1 == p2)
+		return true;
+	else
+		return false;
+
+}
+
 void init()
 {
-	memset(visit,0,sizeof(visit));
-
 	return;
 }
 
 
 void sortFunction()
 {
-	for(int i = 0;i < vertex;i++)
-	{
-		sort(v[i].begin(),v[i].end());
-	}
+	sort(v.begin(),v.end());
 
-	for(int i = 0;i < vertex;i++)
+	cout<<"sort"<<endl;
+	for(int i = 0;i < edge;i++)
         {
-                cout<<v[i].size()<<endl;
-                for(int j = 0;j < v[i].size();j++)
-                {
-                        cout<< get<0>(v[i][j])<<" "<< get<1>(v[i][j])<<" "<< get<2>(v[i][j])<<endl;
-                }
+                cout<<get<0>(v[i])<<" "<<get<1>(v[i])<<" "<<get<2>(v[i])<<" "<<get<3>(v[i])<<endl;
         }
 
 	return;
@@ -62,30 +98,46 @@ void finFunction()
 {
 	fin>>vertex>>edge;
 
-	v.reserve(vertex);
+	v.reserve(edge);
+	save.reserve(edge);
 
 	int t1,t2,t3;
 	for(int i = 0;i < edge;i++)
 	{
 		fin>>t1>>t2>>t3;
-		
-		tu tmp = make_tuple(t3,i,t2);
 
-		v[t1].push_back(tmp);
-
-		tmp = make_tuple(t3,i,t1);
-
-		v[t2].push_back(tmp);
+		tu tmp = make_tuple(t3,i,t1,t2);
+		v.push_back(tmp);
 	}
+
+	return;
+}
+
+void kruskal()
+{
+	sum = 0;
 
 	for(int i = 0;i < vertex;i++)
+		visit[i] = i;
+
+	for(int i = 0;i < edge;i++)
 	{
-		cout<<v[i].size()<<endl;
-		for(int j = 0;j < v[i].size();j++)
+		if( !checkParent( get<2>(v[i]) , get<3>(v[i]) ) )
 		{
-			cout<< get<0>(v[i][j])<<" "<< get<1>(v[i][j])<<" "<< get<2>(v[i][j])<<endl;
+			sum += get<0>(v[i]);
+			
+			save.push_back(get<1>(v[i]));
+			
+			unionParent( get<2>(v[i]) , get<3>(v[i]) );
 		}
 	}
+
+	fout<<"Tree edges by Kruskal algorithm: "<<sum<<endl;
+	for(int i = 0;i < save.size();i++)
+		fout<<save[i]<<endl;
+
+	save.clear();
+
 	return;
 }
 
@@ -93,11 +145,11 @@ int main()
 {
 	start_t = clock();
 
-	init();
-
 	finFunction();
 
 	sortFunction();
+
+	kruskal();
 
 	fin.close();
 	fout.close();
