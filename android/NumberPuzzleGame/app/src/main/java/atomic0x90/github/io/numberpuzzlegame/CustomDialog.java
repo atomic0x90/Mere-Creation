5e428fcce28007f96341ef75877eb663614c46f8
atomic0x90 <yoojun20@gmail.com>
2021-01-08 23:30:46 +0900

    Add adCheck

--- android/NumberPuzzleGame/app/src/main/java/atomic0x90/github/io/numberpuzzlegame/CustomDialog.java
+++ android/NumberPuzzleGame/app/src/main/java/atomic0x90/github/io/numberpuzzlegame/CustomDialog.java
@@ -3,6 +3,10 @@ package atomic0x90.github.io.numberpuzzlegame;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
@@ -13,6 +17,8 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 
+import java.io.File;
+
 public class CustomDialog extends Dialog{
     private Context mContext;
 
@@ -20,6 +26,12 @@ public class CustomDialog extends Dialog{
         super(context);
         mContext = context;
     }
+
+
+    //DB
+    SQLiteDatabase sqLiteDatabase;
+
+    int adCheck;
     //Sound
     SoundPool soundPool;
     int soundID;
@@ -37,6 +49,8 @@ public class CustomDialog extends Dialog{
 
         getWindow().setAttributes(lp);
 
+        sqLiteDatabase = init_database();
+        updateAdCheck();
 
         //Sound
         soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC,0);
@@ -50,7 +64,55 @@ public class CustomDialog extends Dialog{
 
                 CustomDialog.this.dismiss();
                 ((Activity)mContext).finish();
+                if(adCheck % 5 == 0){
+                    Intent intent = new Intent(getContext(),SplashScreen.class);
+                    getContext().startActivity(intent);
+                }
             }
         });
+    }
+
+    //DB
+    private SQLiteDatabase init_database() {
+
+        SQLiteDatabase db = null ;
+        // File file = getDatabasePath("contact.db") ;
+        File file = new File(getContext().getFilesDir(), "contact.db") ;
+
+        System.out.println("PATH : " + file.toString()) ;
+        try {
+            db = SQLiteDatabase.openOrCreateDatabase(file, null) ;
+        } catch (SQLiteException e) {
+            e.printStackTrace() ;
+        }
+
+        if (db == null) {
+            System.out.println("DB creation failed. " + file.getAbsolutePath()) ;
+        }
+
+        return db ;
+    }
+
+
+    private void load_AdCheck(){
+        if(sqLiteDatabase != null){
+            String sqlQuery = "SELECT * FROM AdCheck";
+            Cursor cursor = null;
+
+            cursor = sqLiteDatabase.rawQuery(sqlQuery,null);
+
+            if(cursor.moveToNext()) {
+                adCheck = cursor.getInt(0);
+            }
+        }
+    }
+
+    private void updateAdCheck(){
+        if(sqLiteDatabase != null){
+            load_AdCheck();
+            adCheck += 1;
+            String sqlUpdateCoin = "UPDATE AdCheck SET adCheck=" + adCheck;
+            sqLiteDatabase.execSQL(sqlUpdateCoin);
+        }
     }
 }
