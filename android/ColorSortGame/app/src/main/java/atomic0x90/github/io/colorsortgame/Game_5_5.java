c95c75e07d011a16a215bcb5d239571217da3f64
atomic0x90 <yoojun20@gmail.com>
2021-02-05 22:07:53 +0900

    Init 5_5

---
+++ android/ColorSortGame/app/src/main/java/atomic0x90/github/io/colorsortgame/Game_5_5.java
@@ -0,0 +1,14 @@
+package atomic0x90.github.io.colorsortgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Game_5_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_5_5);
+    }
+}
