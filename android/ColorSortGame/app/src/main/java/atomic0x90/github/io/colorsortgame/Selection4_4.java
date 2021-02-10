692b39cb9085a1bd698e7022ce89e29d4a4b34dc
atomic0x90 <yoojun20@gmail.com>
2021-02-10 14:48:51 +0900

    Test handler

--- android/ColorSortGame/app/src/main/java/atomic0x90/github/io/colorsortgame/Selection4_4.java
+++ android/ColorSortGame/app/src/main/java/atomic0x90/github/io/colorsortgame/Selection4_4.java
@@ -3,14 +3,32 @@ package atomic0x90.github.io.colorsortgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
 
 public class Selection4_4 extends AppCompatActivity {
+    @Override
+    protected void onStart(){
+        super.onStart();
 
+        final Button button1 = (Button) findViewById(R.id.button18);
+        button1.setEnabled(false);
+        class startHandler implements Runnable{
+
+            public void run(){
+                button1.setEnabled(true);
+            }
+        }
+
+        Handler h = new Handler();
+        h.postDelayed(new startHandler(),250);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection4_4);
     }
+
     @Override
     protected void onUserLeaveHint(){
         super.onUserLeaveHint();
