f034828b4c5b1bf983215fd7275530f3a85a56b7
atomic0x90 <yoojun20@gmail.com>
2021-02-28 13:44:01 +0900

    Init sqlDB

--- android/ColorSortGame/app/src/main/java/atomic0x90/github/io/colorsortgame/MainActivity.java
+++ android/ColorSortGame/app/src/main/java/atomic0x90/github/io/colorsortgame/MainActivity.java
@@ -3,11 +3,16 @@ package atomic0x90.github.io.colorsortgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase sqliteDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +46,33 @@ public class MainActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
             }
         });
+    }
+
+    //DB
+    private SQLiteDatabase init_database() {
+
+        SQLiteDatabase db = null ;
+        // File file = getDatabasePath("contact.db") ;
+        File file = new File(getFilesDir(), "contact.db") ;
+
+        System.out.println("PATH : " + file.toString()) ;
+        try {
+            db = SQLiteDatabase.openOrCreateDatabase(file, null) ;
+        } catch (SQLiteException e) {
+            e.printStackTrace() ;
+        }
+
+        if (db == null) {
+            System.out.println("DB creation failed. " + file.getAbsolutePath()) ;
+        }
+
+        return db ;
+    }
+
+    private void init_tables() {
+        if(sqliteDB != null){
+
+            
+        }
     }
 }
