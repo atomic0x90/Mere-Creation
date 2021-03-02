bfb5d6a9049e9ef87de74031b5921f38cb654d5a
atomic0x90 <yoojun20@gmail.com>
2021-03-02 22:20:44 +0900

    Add screen size

--- android/ColorSortGame/app/src/main/java/atomic0x90/github/io/colorsortgame/Game_10_10_single.java
+++ android/ColorSortGame/app/src/main/java/atomic0x90/github/io/colorsortgame/Game_10_10_single.java
@@ -2,10 +2,29 @@ package atomic0x90.github.io.colorsortgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
 public class Game_10_10_single extends AppCompatActivity {
+    int standardSize_X, standardSize_Y;
+    float density;
+    public Point getScreenSize(Activity activity) {
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
 
+        return size;
+    }
+
+    public void getStandardSize() {
+        Point ScreenSize = getScreenSize(this);
+        density = getResources().getDisplayMetrics().density;
+
+        standardSize_X = (int) (ScreenSize.x / density);
+        standardSize_Y = (int) (ScreenSize.y / density);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
