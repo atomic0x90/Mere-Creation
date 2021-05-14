08cd1bdf1c749b2d14226794c72076239c1b9095
atomic0x90 <hanyujun.study@gmail.com>
2021-05-14 15:07:48 +0900

    Change c,r

--- algorithmProblem/colorGameAlgo/xmlButton4/xml4.cpp
+++ algorithmProblem/colorGameAlgo/xmlButton4/xml4.cpp
@@ -7,42 +7,14 @@ ofstream fout("xml.out");
 
 int main(){
 	int c,r;
+	int check = 0;
 	for(int i = 0;i < 50;i++){
-		if((i+1) % 4 == 1)
+		if((i+1) % 2 == 1)
 			c = 0;
-		else if((i+1) % 4 == 2)
+		else if((i+1) % 2 == 0)
 			c = 1;
-		else if((i+1) % 4 == 3)
-			c = 2;
-		else if((i+1) % 4 == 0)
-			c = 3;
-
-		if(i < 4)
-			r = 0;
-		else if(i < 8)
-			r = 1;
-		else if(i < 12)
-			r = 2;
-		else if(i < 16)
-			r = 3;
-		else if(i < 20)
-			r = 4;
-		else if(i < 24)
-			r = 5;
-		else if(i < 28)
-			r = 6;
-		else if(i < 32)
-			r = 7;
-		else if(i < 36)
-			r = 8;
-		else if(i < 40)
-			r = 9;
-		else if(i < 44)
-			r = 10;
-		else if(i < 48)
-			r = 11;
-		else if(i < 52)
-			r = 12;
+		r = i / 2;
+		
 		fout<<"<Button"<<endl;
 		fout<<"\tandroid:id=\"@+id/selection4_4_"<<i+1<<"\""<<endl;
 		fout<<"\tandroid:layout_width=\"wrap_content\""<<endl;
